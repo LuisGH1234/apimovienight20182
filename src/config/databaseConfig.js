3d897444ef1f518c1ec476bd7ef9d61dad934c19
LuisGH1234 <u201516808@upc.edu.pe>
2018-09-25 12:30:30 -0500

    api al 98% +

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,17 +1,17 @@
-/*const credentials = {};
+const credentials = {};
 
 credentials.user = "b071488dca2501";
 credentials.password = "ec20c0a1";
 credentials.port = 3306;
 credentials.databaseName = "heroku_f1cf93086df67b3";
 credentials.host_name = "us-cdbr-iron-east-01.cleardb.net";
-*/
+
 //username: b071488dca2501
 //password: ec20c0a1
 //hostname: us-cdbr-iron-east-01.cleardb.net
 //database name: heroku_f1cf93086df67b3
 
-class Credentials {
+/*class Credentials {
     constructor(local){
         this.local = local || true;
     }
@@ -46,6 +46,6 @@ class Credentials {
         }
         return "us-cdbr-iron-east-01.cleardb.net";
     }
-}
+}*/
 
-module.exports = Credentials;
+module.exports = credentials;
