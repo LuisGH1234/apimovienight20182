07b56a7bf431bd4e0792f1d17d05439184f10e04
LuisGH1234 <u201516808@upc.edu.pe>
2018-10-30 22:40:05 -0500

    test 11

--- src/routes/user/user_route.js
+++ src/routes/user/user_route.js
@@ -24,7 +24,7 @@ router.get('/users', userController.getUsers);
 router.get('/users/:id', auth.isAuth, userController.getUser);
 router.put('/users/:id', auth.isAuth, userController.updateUser);
 
-router.post('/siginup', /*usersign.signUp*/ (req,res) => {
+router.post('/signup', (req,res) => {
     res.json({ active: 'true' });
 });
 router.post('/login', usersign.singIn);
