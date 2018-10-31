08a41377dc0cfdb804a513b688570561414089eb
LuisGH1234 <u201516808@upc.edu.pe>
2018-10-30 22:29:09 -0500

    test 7

--- src/routes/user/user_route.js
+++ src/routes/user/user_route.js
@@ -25,7 +25,7 @@ router.get('/private', auth.isAuth, auth.AccessDone);
 router.get('/users/:id', auth.isAuth, userController.getUser);
 router.put('/users/:id', auth.isAuth, userController.updateUser);
 
-router.post('/signup', usersign.signUp);
+router.post('/signUp', usersign.signUp);
 router.post('/signin', usersign.singIn);
 
 router.get('/users/:user_id/personal_playlists/:p_play_id/personal_media_contents', auth.isAuth, personalMediaContentController.getPersonalMediaContents);
